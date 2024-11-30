9ca9f2bb68ccc554f7b89ab46ce77aef4cc3a3c2
Ramisa_Raidah <ramisaraidah@gmail.com>
2024-12-01 01:39:28 +0600

    Changes to be committed: 	new file:   Accessory/Coins/Bronze_1.bmp 	new file:   Accessory/Coins/Bronze_10.bmp 	new file:   Accessory/Coins/Bronze_2.bmp 	new file:   Accessory/Coins/Bronze_3.bmp 	new file:   Accessory/Coins/Bronze_4.bmp 	new file:   Accessory/Coins/Bronze_5.bmp 	new file:   Accessory/Coins/Bronze_6.bmp 	new file:   Accessory/Coins/Bronze_7.bmp 	new file:   Accessory/Coins/Bronze_8.bmp 	new file:   Accessory/Coins/Bronze_9.bmp 	new file:   Accessory/Coins/Gold_1.bmp 	modified:   Sample.cpp 	modified:   iMain.cpp 	modified:   iMain.exe

--- Sample.cpp
+++ Sample.cpp
@@ -1,276 +1,136 @@
-# include "iGraphics.h"
+#include "iGraphics.h"
 
-int gameState=0;
+#define screenwidth 619
+#define screenlength 1100
 
-
-
+bool run_state;
+int gameState = 0;
 bool musicOn = true;
+int collision_number = 0;
+
+/*-----COORDINATES--------*/
+int X = 300;
+int Y = 55;
+int playerWidth = 52, playerHeight = 112;
+
 /*----------- Background --------- */ 
-char bc[22][20]={"back-55\\tile000.bmp","back-55\\tile001.bmp","back-55\\tile002.bmp","back-55\\tile003.bmp","back-55\\tile004.bmp","back-55\\tile005.bmp","back-55\\tile006.bmp","back-55\\tile007.bmp","back-55\\tile008.bmp","back-55\\tile009.bmp","back-55\\tile010.bmp","back-55\\tile011.bmp","back-55\\tile012.bmp","back-55\\tile013.bmp","back-55\\tile014.bmp","back-55\\tile015.bmp","back-55\\tile016.bmp","back-55\\tile017.bmp","back-55\\tile018.bmp","back-55\\tile019.bmp"};
-char stat_back[20]={"back.bmp"};
+char bc[22][20] = {"back-55\\tile000.bmp", "back-55\\tile001.bmp", ...};
+char stat_back[20] = {"back.bmp"};
 
-/*----------- Animations of main character--------- */ 
-char idle[6][25]={"character\\Idle\\1.bmp","character\\Idle\\2.bmp","character\\Idle\\3.bmp","character\\Idle\\4.bmp","character\\Idle\\5.bmp","character\\Idle\\6.bmp"};
-char run[8][25]={"character\\Run\\1.bmp","character\\Run\\2.bmp","character\\Run\\3.bmp","character\\Run\\4.bmp","character\\Run\\5.bmp","character\\Run\\6.bmp","character\\Run\\7.bmp","character\\Run\\8.bmp"};
-char jump[11][25]={"character\\jump\\1.bmp","character\\jump\\2.bmp","character\\jump\\3.bmp","character\\jump\\4.bmp","character\\jump\\5.bmp","character\\jump\\6.bmp","character\\jump\\7.bmp","character\\jump\\8.bmp","character\\jump\\9.bmp","character\\jump\\10.bmp","character\\jump\\11.bmp"};
-char hurt[4][25]={"character\\hurt\\1.bmp","character\\hurt\\2.bmp","character\\hurt\\3.bmp","character\\hurt\\4.bmp"};
-char attack[10][25]={"character\\Attack\\1.bmp","character\\Attack\\2.bmp","character\\Attack\\3.bmp","character\\Attack\\4.bmp","character\\Attack\\5.bmp","character\\Attack\\6.bmp","character\\Attack\\7.bmp","character\\Attack\\8.bmp","character\\Attack\\9.bmp","character\\Attack\\10.bmp"};
+/*----------- Animations -----------*/
+char idle[6][25] = {"character\\Idle\\1.bmp", "character\\Idle\\2.bmp", ...};
+char run[8][25] = {"character\\Run\\1.bmp", "character\\Run\\2.bmp", ...};
+char jump_sprite[11][25] = {"character\\jump\\1.bmp", "character\\jump\\2.bmp", ...};
+char enemy_sprite[3][60] = {"enemy\\green_slime\\idle\\spike.bmp", ...};
+char coin[40][35] = {"Accessory\\Coins\\Bronze_1.bmp", "Accessory\\Coins\\Bronze_2.bmp", ...};
 
-/*---------------- UI ------------- */
-char menupage[20]={"UI\\Menu.png"};
+/*------- Coin Data --------*/
+#define MAX_COINS 3
+struct Coin {
+    int x;
+    int y;
+    bool active;
+} coins[MAX_COINS];
 
+int coinidx = 0;
+int totalCoinsCollected = 0;
 
-/*------RUN ANIMATION-----*/
-bool StandPosition = true;
-int StandCounter = 0;
+/*------Obstacle--------*/
+struct Obstacle {
+    int obs_x;
+    int obs_y;
+    int obs_width;
+    int obs_height;
+    bool active;
+} obs1;
 
-/*----------OBSTACLES---------*/
-struct obstacle {
-    int x;        // X position
-    int y;        // Y position
-    int width;    // Width of obstacle
-    int height;   // Height of obstacle
-    bool active;  // Whether the obstacle is active
-};
-struct obstacle obstacles[5]; 
+/*--------Player & Movement--------*/
+bool jump = false, jumpup = false, jumpdown = false;
+int jumplimit = 100, coordinatejump = 0;
 
+/*---------------- Functions ----------------*/
 
-
-struct background
-{
-	int x;
-	int y;
-};
-struct buttonCoordinate
-{
-	int x;
-	int y;
-};
-
-struct background bc1[50];
-
-int bcIndex=0;
-int charx=300;
-int chary=55;
-int idleidx=0,runidx=0,jumpidx=0,hurtidx=0,attackidx=0;
-
-// charx = 300; // Update this in iDraw or wherever the character's position is controlled
-// chary = 55;
-
-
-void generateObstacles();
-void moveObstacles();
-void checkCollision();
-
-
-//	function iDraw() is called again and again by the system.
-void iDraw() {
-	iClear();
-	if(gameState==0 || gameState==-1)
-	{
-		iShowBMP(0,0,menupage);
-		iText(400, 300, "Press UP to Start the Game", GLUT_BITMAP_TIMES_ROMAN_24);
-	}
-	else
-	{
-		iShowBMP(0,0,stat_back);
-		int i,k,j;
-		
-
-		if(!StandPosition)
-		{
-			for(i=0;i<20;i++)
-			{
-			iShowBMP(bc1[i].x,bc1[i].y,bc[i]);
-			}
-			iShowBMP2(300,55,run[runidx],0);
-		}
-		else
-		{
-			iShowBMP2(300,55,idle[idleidx],0);
-		}
-		for (int i = 0; i < 5; i++) {
-            if (obstacles[i].active) {
-                iSetColor(255, 0, 0); // Red for obstacles
-                iFilledRectangle(obstacles[i].x, obstacles[i].y, obstacles[i].width, obstacles[i].height);
-            }
-        }
-		
-		// iShowBMP2(200,55,jump[jumpidx],0);
-		// iShowBMP2(300,55,hurt[hurtidx],0);
-		// iShowBMP2(400,55,attack[attackidx],0);
-	}
-}
-
-
-void setAll()
-{
-	int sum=0;
-	for(int i=0;i<20;i++)
-	{
-		bc1[i].y=0;
-		bc1[i].x=sum;
-		sum+=55;
-		//if(!StandPosition)
-			generateObstacles();  
-	}
-}
-// Function to generate random obstacles
-void generateObstacles() {
-    for (int i = 0; i < 5; i++) {
-        obstacles[i].x = rand() % 1100 + 1100;  // Start position off-screen
-        obstacles[i].y = 55 + rand() % 200;    // Random height on screen
-        obstacles[i].width = 50;              // Fixed width
-        obstacles[i].height = 50 + rand() % 50; // Random height
-        obstacles[i].active = true;
+/* Coin Initialization */
+void initializeCoins() {
+    for (int i = 0; i < MAX_COINS; i++) {
+        coins[i].x = screenlength + rand() % 400;
+        coins[i].y = Y + 50 + rand() % 100; // Random heights
+        coins[i].active = true;
     }
 }
 
-// Function to move obstacles
-void moveObstacles() {
-    for (int i = 0; i < 5; i++) {
-        if (obstacles[i].active) {
-            obstacles[i].x -= 30;  // Move left
-            if (obstacles[i].x + obstacles[i].width <= 0) {
-                // Reset to new random position off-screen
-                obstacles[i].x = rand() % 1100 + 1100;
-                obstacles[i].y = 55 + rand() % 200;
-                obstacles[i].height = 50 + rand() % 50;
+/* Coin Movement */
+void moveCoins() {
+    for (int i = 0; i < MAX_COINS; i++) {
+        if (coins[i].active) {
+            coins[i].x -= 10;
+            if (coins[i].x < 0) {
+                coins[i].active = false;
             }
         }
     }
 }
+
+/* Respawn Inactive Coins */
+void respawnCoins() {
+    for (int i = 0; i < MAX_COINS; i++) {
+        if (!coins[i].active) {
+            coins[i].x = screenlength + rand() % 200 + 300; // Respawn further ahead
+            coins[i].y = Y + 50 + rand() % 100;            // Random height
+            coins[i].active = true;
+        }
+    }
+}
+
+/* Check Coin Collection */
+void checkCoinCollection() {
+    for (int i = 0; i < MAX_COINS; i++) {
+        if (coins[i].active) {
+            if (X + playerWidth > coins[i].x && X < coins[i].x + 30 &&
+                Y + playerHeight > coins[i].y && Y < coins[i].y + 30) {
+                coins[i].active = false;
+                totalCoinsCollected++;
+                printf("Coin collected! Total: %d\n", totalCoinsCollected);
+            }
+        }
+    }
+}
+
+/* Draw Coins */
+void drawCoins() {
+    for (int i = 0; i < MAX_COINS; i++) {
+        if (coins[i].active) {
+            iShowBMP2(coins[i].x, coins[i].y, coin[coinidx], 0);
+        }
+    }
+}
+
+/* Collision Detection */
 void checkCollision() {
-    for (int i = 0; i < 5; i++) {
-        if (obstacles[i].active) {
-            // Check if character's bounding box intersects with the obstacle
-            if (charx < obstacles[i].x + obstacles[i].width &&
-                charx + 50 > obstacles[i].x && // Assume character's width = 50
-                chary < obstacles[i].y + obstacles[i].height &&
-                chary + 50 > obstacles[i].y) { // Assume character's height = 50
-                gameState = -1; // Game Over
+    if (obs1.active && !jump) {
+        if (X < obs1.obs_x + obs1.obs_width && X + playerWidth > obs1.obs_x &&
+            Y < obs1.obs_y + obs1.obs_height && Y + playerHeight > obs1.obs_y) {
+            collision_number++;
+            printf("Collision detected! Total: %d\n", collision_number);
+            if (collision_number > 45) {
+                gameState = 0; // End game if collisions exceed
             }
         }
     }
 }
 
-void change()
-{
-	for(int i=0;i<20;i++)
-	{
-		bc1[i].x-= 30;
-		// if(bc1[i].x<=0)
-		// {
-		// 	bc1[i].x=1100;
-		//}
-		if (bc1[i].x + 55 <= 0) 
-		{
-            bc1[i].x = bc1[(i == 0 ? 19 : i - 1)].x + 55; 
-		}
-	}
-	moveObstacles();
-	checkCollision(); 
+/* Game Setup */
+void setAll() {
+    initializeCoins();
+    set_obstacle();
+    gameState = 1;
 }
 
-
-/*
-	function iMouseMove() is called when the user presses and drags the mouse.
-	(mx, my) is the position where the mouse pointer is.
-	*/
-void iMouseMove(int mx, int my) {
-	//printf("x = %d, y= %d\n",mx,my);
-	//place your codes her
-
-}
-
-/*
-	function iMouse() is called when the user presses/releases the mouse.
-	(mx, my) is the position where the mouse pointer is.
-	*/
-void iMouse(int button, int state, int mx, int my) {
-	
-}
-
-/*
-	function iKeyboard() is called whenever the user hits a key in keyboard.
-	key- holds the ASCII value of the key pressed.
-	*/
-void iKeyboard(unsigned char key) {
-	
-
-	
-}
-
-/*
-	function iSpecialKeyboard() is called whenver user hits special keys like-
-	function keys, home, end, pg up, pg down, arraows etc. you have to use
-	appropriate constants to detect them. A list is:
-	GLUT_KEY_F1, GLUT_KEY_F2, GLUT_KEY_F3, GLUT_KEY_F4, GLUT_KEY_F5, GLUT_KEY_F6,
-	GLUT_KEY_F7, GLUT_KEY_F8, GLUT_KEY_F9, GLUT_KEY_F10, GLUT_KEY_F11, GLUT_KEY_F12,
-	GLUT_KEY_LEFT, GLUT_KEY_UP, GLUT_KEY_RIGHT, GLUT_KEY_DOWN, GLUT_KEY_PAGE UP,
-	GLUT_KEY_PAGE DOWN, GLUT_KEY_HOME, GLUT_KEY_END, GLUT_KEY_INSERT
-	*/
-void iSpecialKeyboard(unsigned char key) {
-	//place your codes for other keys here
-	if (key == GLUT_KEY_UP) {
-		gameState=1;
-	}
-	else if(key==GLUT_KEY_DOWN)
-	{
-		gameState=0;
-	}
-	if(key==GLUT_KEY_RIGHT)
-	{
-		StandPosition= false;
-	}
-	else if(key==GLUT_KEY_LEFT)
-	{
-		StandPosition= true;
-	}
-}
-
-
-void idle_anim()
-{
-	idleidx++;
-	if(idleidx>=6) idleidx=0;
-}
-void run_anim()
-{
-	runidx++;
-	if(runidx>=8) runidx=0;
-}
-
-void jump_anim()
-{
-	jumpidx++;
-	if(jumpidx>=11) jumpidx=0;
-}
-
-void hurt_anim()
-{
-	hurtidx++;
-	if(hurtidx>=4) hurtidx=0;
-}
-void attack_anim()
-{
-	attackidx++;
-	if(attackidx>=10) attackidx=0;
-}
-
-char musicfile[100]={"Resouces\\music\\nightfall-future-bass-music-228100.wav"};
 int main() {
-	setAll();
-	if(musicOn)
-	{
-		//PlaySound( musicfile , NULL, SND_LOOP | SND_ASYNC);
-	}
-	iSetTimer(100,change);
-	iSetTimer(100,idle_anim);
-	iSetTimer(100,run_anim);
-	iSetTimer(200,jump_anim);
-	iSetTimer(200,hurt_anim);
-	iSetTimer(100,attack_anim);
-	iInitialize(1100,620, "Spooky Sprints");
-	return 0;
+    srand(time(0));
+    setAll();
+    iSetTimer(100, moveCoins);
+    iSetTimer(200, respawnCoins);
+    iInitialize(screenlength, screenwidth, "Spooky Sprints");
+    return 0;
 }
--- iMain.cpp
+++ iMain.cpp
@@ -11,21 +11,38 @@ int collision_number=0;
 /*-----COORDINATES--------*/
 int X=300;
 int Y=55;
+int playerWidth=52, playerHeight=112;
+
 
 /*----------- Background --------- */ 
 char bc[22][20]={"back-55\\tile000.bmp","back-55\\tile001.bmp","back-55\\tile002.bmp","back-55\\tile003.bmp","back-55\\tile004.bmp","back-55\\tile005.bmp","back-55\\tile006.bmp","back-55\\tile007.bmp","back-55\\tile008.bmp","back-55\\tile009.bmp","back-55\\tile010.bmp","back-55\\tile011.bmp","back-55\\tile012.bmp","back-55\\tile013.bmp","back-55\\tile014.bmp","back-55\\tile015.bmp","back-55\\tile016.bmp","back-55\\tile017.bmp","back-55\\tile018.bmp","back-55\\tile019.bmp"};
 char stat_back[20]={"back.bmp"};
 
+
 /*----------- Animations of main character--------- */
-int playerWidth=52, playerHeight=112;
 char idle[6][25]={"character\\Idle\\1.bmp","character\\Idle\\2.bmp","character\\Idle\\3.bmp","character\\Idle\\4.bmp","character\\Idle\\5.bmp","character\\Idle\\6.bmp"};
 char run[8][25]={"character\\Run\\1.bmp","character\\Run\\2.bmp","character\\Run\\3.bmp","character\\Run\\4.bmp","character\\Run\\5.bmp","character\\Run\\6.bmp","character\\Run\\7.bmp","character\\Run\\8.bmp"};
 char jump_sprite[11][25]={"character\\jump\\1.bmp","character\\jump\\2.bmp","character\\jump\\3.bmp","character\\jump\\4.bmp","character\\jump\\5.bmp","character\\jump\\6.bmp","character\\jump\\7.bmp","character\\jump\\8.bmp","character\\jump\\9.bmp","character\\jump\\10.bmp","character\\jump\\11.bmp"};
 char hurt[4][25]={"character\\hurt\\1.bmp","character\\hurt\\2.bmp","character\\hurt\\3.bmp","character\\hurt\\4.bmp"};
 char attack[10][25]={"character\\Attack\\1.bmp","character\\Attack\\2.bmp","character\\Attack\\3.bmp","character\\Attack\\4.bmp","character\\Attack\\5.bmp","character\\Attack\\6.bmp","character\\Attack\\7.bmp","character\\Attack\\8.bmp","character\\Attack\\9.bmp","character\\Attack\\10.bmp"};
 
+
 /*--------ANIMATIONS FOR THE OBSTABLES---------*/
 char enemy_sprite[3][60]={"enemy\\green_slime\\idle\\spike.bmp","enemy\\green_slime\\idle\\frame-1.bmp","enemy\\green_slime\\idle\\frame-2.bmp"};
+
+
+/*--------------------------------Coins---------------------------------*/
+#define MAX_COINS 3
+int coinidx=0;
+int totalCoinsCollected = 0;
+int coinTime=0;
+char coin[40][35]={"Accessory\\Coins\\Bronze_1.bmp","Accessory\\Coins\\Bronze_2.bmp","Accessory\\Coins\\Bronze_3.bmp","Accessory\\Coins\\Bronze_4.bmp","Accessory\\Coins\\Bronze_5.bmp","Accessory\\Coins\\Bronze_6.bmp","Accessory\\Coins\\Bronze_7.bmp","Accessory\\Coins\\Bronze_8.bmp","Accessory\\Coins\\Bronze_9.bmp","Accessory\\Coins\\Bronze_10.bmp"};
+struct Coin {
+    int x;       // X-coordinate
+    int y;       // Y-coordinate
+    bool active; // Whether the coin is active
+};
+struct Coin coins[MAX_COINS];
 
 /*---------------- UI ------------- */
 char menupage[20]={"UI\\Menu.png"};
@@ -65,8 +82,10 @@ int idleidx=0,runidx=0,jumpupidx=0,jumpdownidx=7,hurtidx=0,attackidx=0;
 
 /*-------Introduce Functions----------*/
 void checkCollision();
+void checkCoinCollection();
+void drawCoins();
 
-
+/*----------------    i draw   --------------------- I DRAW --------------------------------------------*/
 void iDraw() {
 	iClear();
 	if(gameState==0 || gameState==-1)
@@ -76,6 +95,14 @@ void iDraw() {
 	}
 	else if(gameState==1)
 	{
+		if(coins->active)
+		{
+			printf("\n\nACTIVE\n\n");
+		}
+		else
+		{
+			printf("\n\nINACTIVE\n\n");
+		}
 		int i,k,j;
 		iShowBMP(0,0,stat_back);
 		for(i=0;i<20;i++)
@@ -112,6 +139,24 @@ void iDraw() {
 		{
 			iShowBMP2(obs1.obs_x,obs1.obs_y,enemy_sprite[0],0);
 		}
+		drawCoins();
+
+        // Display Total Coins Collected
+        char coinText[50];
+        sprintf(coinText, "Coins Collected: %d", totalCoinsCollected);
+        iText(10, screenwidth - 20, coinText, GLUT_BITMAP_HELVETICA_18);
+
+        // Check for coin collection
+        checkCoinCollection();
+
+        if (obs1.active) {
+            iShowBMP2(obs1.obs_x, obs1.obs_y, enemy_sprite[0], 0);
+        }
+
+
+		char collisionText[50];
+        sprintf(collisionText, "	Lives remaing: %d", 3-(collision_number/15));
+        iText(900, screenwidth - 20, collisionText, GLUT_BITMAP_HELVETICA_18);
 		checkCollision();	
 	}
 	
@@ -208,11 +253,78 @@ void checkCollision()
 			++collision_number;
 			printf("Collision no: %d\n",collision_number);
            	//gameState = 0;
-			if(collision_number>3)
+			if(collision_number> 3*15)
 			{	
 				gameState=0;
 			}
 			printf("Collision detected!\n");
+        }
+    }
+}
+
+
+
+
+/*-------------------------Coin Collection------------------------*/
+void initializeCoins() {
+    for (int i = 0; i < MAX_COINS; i++) {
+        coins[i].x = screenlength + rand() % 400;;  
+        coins[i].y = Y; 
+        coins[i].active = true;
+    }
+}
+void moveCoins() {
+    for (int i = 0; i < MAX_COINS; i++) {
+        if (coins[i].active) {
+            coins[i].x -= 10; 
+            if (coins[i].x < 0) {
+                coins[i].x = screenlength + rand() % 200; 
+                coins[i].y = Y;
+            }
+        }
+    }
+}
+void checkCoinCollection() {
+    for (int i = 0; i < MAX_COINS; i++) {
+        if (coins[i].active) {
+            if (X + playerWidth > coins[i].x && X < coins[i].x + 30 && Y + playerHeight > coins[i].y && Y < coins[i].y + 30) {
+                coins[i].active = false; 
+                totalCoinsCollected++; 
+                printf("Coin collected at (%d, %d)! Total: %d\n", coins[i].x, coins[i].y, totalCoinsCollected);
+            }
+        }
+		else
+		{
+			++coinTime;
+		}
+		if(coinTime>20)
+		{
+			coinTime=0;
+			for (int i = 0; i < MAX_COINS; i++) {
+				coins[i].x = rand() % screenlength + X +10;
+				coins[i].y = Y;
+				coins[i].active = true;
+	        }
+    	}
+			
+    }
+}
+
+void respawnCoins() {
+	
+    for (int i = 0; i < MAX_COINS; i++) {
+
+        if (!coins[i].active) {
+            coins[i].x = rand() % screenlength + X +10;
+            coins[i].y = Y;
+            coins[i].active = true;
+        }
+    }
+}
+void drawCoins() {
+    for (int i = 0; i < MAX_COINS; i++) {
+        if (coins[i].active) {
+            iShowBMP2(coins[i].x, coins[i].y, coin[coinidx], 0);
         }
     }
 }
@@ -312,9 +424,15 @@ void attack_anim()
 	attackidx++;
 	if(attackidx>=10) attackidx=0;
 }
+void coin_anim()
+{
+	coinidx++;
+	if(coinidx>=8) coinidx=0;
+}
 
 char musicfile[100]={"Resouces\\music\\nightfall-future-bass-music-228100.wav"};
 int main() {
+	srand(0);
 	setAll();
 	set_obstacle();
 	iSetTimer(300,generate_obstacle);
@@ -324,11 +442,15 @@ int main() {
 		//PlaySound( musicfile , NULL, SND_LOOP | SND_ASYNC);
 	}
 	iSetTimer(100,change);
+	iSetTimer(100,coin_anim);
 	iSetTimer(100,idle_anim);
 	iSetTimer(100,run_anim);
 	iSetTimer(5,jumping);
 	iSetTimer(50,jumpup_anim);
 	iSetTimer(50,jumpdown_anim);
+	initializeCoins();
+	iSetTimer(100, moveCoins);        
+    iSetTimer(100, respawnCoins);
 	iInitialize(screenlength,screenwidth, "Spooky Sprints");
 	return 0;
 }
